b1e731789f8a83366b4337abb7a15bca74591086
Garrett Johnson <gjohnson2718@gmail.com>
2016-02-01 12:32:18 -0500

    This somehow morphed into an input demo, and it didn't crash and burn. Perhaps in time, I could have a text based game on my hands.

---
+++ src/Foo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by garrett on 2/1/16.
+ */
+public class Foo {
+    int calcArea(int h, int w){
+        return h*w;
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,14 @@
 /**
  * Created by garrett on 2/1/16.
  */
+import java.util.Scanner;
 public class Runner {
     public static void main(String[] args){
+        Scanner user_input= new Scanner(System.in);
+        String input =user_input.next();
+        System.out.println(input);
 
+
+        }
     }
-}
+
