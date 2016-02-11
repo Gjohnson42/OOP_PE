bf01ad2c79f03dfb83d9a04f1c1d62dd3bfe502f
Garrett Johnson <gjohnson2718@gmail.com>
2016-02-10 20:25:48 -0500

    wrote in the randomizer method, but it's a bit weird, and must be debugged.

--- src/Runner.java
+++ src/Runner.java
@@ -1,18 +1,17 @@
 /**
- * Created by garrett on 2/1/16.
+ * Created by garrett on 2/10/16.
  */
+import java.util.Scanner;
 public class Runner {
-    public static void main(String[] args){
-        SimpleDotCom one= new SimpleDotCom();
-        int [] locations = {2,3,4};
-        one.setLocationArray(locations);
-        if(one.checkYourself("2")=="hit"){
-            System.out.println("The method works as intended");
-        }else{
-            System.out.println("Nope. Something went wrong.");
-            System.out.println();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        SimpleDotCom hans = new SimpleDotCom();
+        //just a little testcode for tossup array. delete it when you know it works
+        hans.setLocationArray();
+        for(int testIteration:hans.getLocationCells()){
+            System.out.println(hans.getLocationCells()[testIteration]);
+            testIteration++;
         }
-
 
 
 
--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -4,11 +4,30 @@
 public class SimpleDotCom {
     int[] locationCells;
     int numHits = 0;
-
-    void setLocationArray(int[] loci) {
-        locationCells = loci;
+    int randomWithRange(int min, int max){
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+    int [] tossupArray(){
+        int [] toss = new int [3];
+        //the magic number here is the max and min
+        int randArrayStart =randomWithRange(0,4);
+        //now to use this as a startpoint to iterate through and set the rest of the array, setting it as need be.
+        for (int currentPt: toss){
+            toss[currentPt]=randArrayStart+currentPt;
+            currentPt++;
+        }
+        return toss;
     }
 
+    void setLocationArray() {
+        locationCells=tossupArray();
+    }
+   int[] getLocationCells(){return locationCells;}
+
+    int getNumHits(){
+        return  numHits;
+    }
     String checkYourself(String guess) {
         String result = "miss";
         int guessInt = Integer.parseInt(guess);
@@ -19,12 +38,9 @@ public class SimpleDotCom {
                 numHits++;
                 break;
             }
-
         }
         if (numHits == 3) {
             System.out.println("You have killed this upstart!");
-
-
         }
         return result;
 
