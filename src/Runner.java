1f1a02fbebe946a8581c75d3ab72d5e8b57031bd
Garrett Johnson <gjohnson2718@gmail.com>
2016-02-15 18:55:57 -0500

    Adding in library functionality to fix bugs. not working as of yet

--- src/Runner.java
+++ src/Runner.java
@@ -1,16 +1,32 @@
+import java.util.ArrayList;
+
 /**
  * Created by garrett on 2/10/16.
  */
-
+import java.util.ArrayList;
 public class Runner {
+    int randomWithRange(int min, int max) {
+        int range = (max - min) + 1;
+        return (int) (Math.random() * range) + min;
+    }
+    ArrayList determineArrayList(){
+        int x= randomWithRange(0,4);
+        ArrayList chosenOne= new ArrayList<String>();
+        chosenOne.add(0,x);
+        chosenOne.add(1,x+1);
+        chosenOne.add(2,x+1);
+        return chosenOne;
+    }
+
     public static void main(String[] args) {
         SimpleDotCom hans = new SimpleDotCom();
         ReadyBakeInput inny= new ReadyBakeInput();
+
         boolean isAlive= true;
         System.out.println("Hans the dotcom yet lives. Make him run! Enter numbers 0-6 to hit him. 3 consecutive hits will kill him. ");
         while (isAlive==true){
             //now begins the great gameplay loop
-            hans.setLocationArray();
+
             String playerGuess= inny.getUserInput("Enter a number 0-6: ");
             String result= hans.checkYourself(playerGuess);
             System.out.println(result);
--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,60 +1,34 @@
 /**
  * Created by garrett on 2/2/16.
  */
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 public class SimpleDotCom {
-    int[] locationCells;
-    int numHits = 0;
-    int randomWithRange(int min, int max){
-        int range = (max - min) + 1;
-        return (int)(Math.random() * range) + min;
-    }
-    int [] tossupArray(){
-        int [] toss = new int [4];
-        //the magic number here is the max and min
-        int randArrayStart =randomWithRange(0,4);
-        //now to use this as a startpoint to iterate through and set the rest of the array, setting it as need be.
-        int x=0;
-        while (x<toss.length){
-            toss[x]=randArrayStart+x;
-            x++;
-        }
-        return toss;
-    }
-
-    void setLocationArray() {
-        locationCells=tossupArray();
-    }
-   int[] getLocationCells(){return locationCells;}
-    void displayLocation(){
-        System.out.println(locationCells[0]);
-        System.out.println(locationCells[1]);
-        System.out.println(locationCells[2]);
+    private ArrayList locationCells;
 
 
+
+    void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
     }
 
-    int getNumHits(){
-        return  numHits;
-    }
     String checkYourself(String guess) {
         String result = "miss";
-        int guessInt = Integer.parseInt(guess);
-        for (int cell : locationCells) {
-            if (cell == guessInt) {
-                result = "hit";
-                numHits++;
-                break;
+        for( locationCells){
+
+        }
+       int index=locationCells.indexOf(guess);
+        if(index>=0){
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()){
+                result="kill";
             }
             else{
-                result= "miss";
+                result="hit";
             }
-        }
-        if (numHits == 3) {
-
-            result="kill";
-
+            //end else
         }
         return result;
-
     }
 }
