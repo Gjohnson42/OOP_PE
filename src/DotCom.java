4c591bf38e0a1836f2d225e287ed3b2632ee1a8d
Garrett Johnson <gjohnson2718@gmail.com>
2016-02-18 11:16:15 -0500

    Moving to full scale game. methods not implemented as of yet. writing test code.

--- src/DotCom.java
+++ src/DotCom.java
@@ -1,30 +1,39 @@
 /**
  * Created by garrett on 2/2/16.
  */
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-public class SimpleDotCom {
-    private ArrayList locationCells;
-
+public class DotCom {
+    private String name;
+    private ArrayList<String> locationCells;
+    DotCom(String NameEntry){
+        name= NameEntry;
+    }
+    //tells me the first location in the array
+    void testArray(){
+        System.out.println(locationCells.get(0));
+    }
 
 
     void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
     }
+    //might not be needed, because I set the name in the constructor
+    void setname(String nameIn){
+        name=nameIn;
+    }
+    String getName(){
+        return name;
+    }
 
     String checkYourself(String guess) {
         String result = "miss";
-        for( locationCells){
-
-        }
        int index=locationCells.indexOf(guess);
         if(index>=0){
             locationCells.remove(index);
 
             if (locationCells.isEmpty()){
                 result="kill";
-            }
-            else{
+            } else{
                 result="hit";
             }
             //end else
--- src/Runner.java
+++ src/Runner.java
@@ -3,43 +3,29 @@ import java.util.ArrayList;
 /**
  * Created by garrett on 2/10/16.
  */
-import java.util.ArrayList;
+
 public class Runner {
-    int randomWithRange(int min, int max) {
-        int range = (max - min) + 1;
-        return (int) (Math.random() * range) + min;
-    }
-    ArrayList determineArrayList(){
-        int x= randomWithRange(0,4);
-        ArrayList chosenOne= new ArrayList<String>();
-        chosenOne.add(0,x);
-        chosenOne.add(1,x+1);
-        chosenOne.add(2,x+1);
-        return chosenOne;
-    }
-
-    public static void main(String[] args) {
-        SimpleDotCom hans = new SimpleDotCom();
-        ReadyBakeInput inny= new ReadyBakeInput();
-
-        boolean isAlive= true;
-        System.out.println("Hans the dotcom yet lives. Make him run! Enter numbers 0-6 to hit him. 3 consecutive hits will kill him. ");
-        while (isAlive==true){
-            //now begins the great gameplay loop
-
-            String playerGuess= inny.getUserInput("Enter a number 0-6: ");
-            String result= hans.checkYourself(playerGuess);
-            System.out.println(result);
-            if (result=="kill"){
-                isAlive=false;
-                System.out.println("Now, hans must run!");
-            }
-
-        }
-
-        }
-
-
+    //These are all the variable and object instantiation for this class.
+    private ReadyBakeInput helper = new ReadyBakeInput();
+    private Runner mainGame= new Runner();
+    private ArrayList<DotCom> dotComholder= new ArrayList<>();
+    int numGuesses=0;
+    void setUpGame(){
+        DotCom pets= new DotCom("Pets.com");
+        DotCom jazzercise= new DotCom("Jazzercise.com");
+        DotCom go2=new DotCom("Go2.com");
+        System.out.println("Three Dotcoms lie in wait, but not for long. Enter letters and Numbers to find them.");
 
     }
+    void startPlaying(){
+
+    }
+    void checkUserGuess(){
+
+    }
+    void finishGame(){
+
+    }
+
+}
 
